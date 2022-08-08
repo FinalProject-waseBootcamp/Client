0fb69371b9e8d58930aa5b45b3d0f92dbf5f6e68
OraBenesh <orabenesh@gmail.com>
2022-08-08 17:39:45 +0300

    login with google

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import User from './components/Users'
-
+import User from "./components/Users";
+import SuperLogin from "./components/SuperLogin";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-        <Route path="" element={<User/>} />
+          <Route path="/user" element={<User />} />
+          <Route path="/login" element={<SuperLogin />} />
         </Routes>
       </Router>
-
     </div>
   );
 }
