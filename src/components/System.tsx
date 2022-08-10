33a749fe3c9b0ea3df78885c89bf4bfdbb4d2396
Ester-Tofik <325202349@mby.co.il>
2022-08-10 17:02:49 +0300

    fix get busisses

--- src/components/System.tsx
+++ src/components/System.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { useParams } from "react-router";
 import { Button } from '@mui/material';
+import { useNavigate } from 'react-router';
 export default function System() {
-  // const [searchParams, setSearchParams] = useSearchParams();
-  // const systemName=searchParams.get('name');
-  const { uid,name} = useParams();
-    
+  const { name,uid} = useParams();
+    const navigate= useNavigate();
   return (
     <>
     <h1>hellow to {name} system</h1>
-    {/* {admin&&<Button onClick={()=>navigate('/mySystem')}>my systems</Button>} */}
+    <Button variant="contained" onClick={()=>navigate('/mySystem')} >My system</Button>
     </>
   );
 }
