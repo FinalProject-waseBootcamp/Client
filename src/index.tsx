2b7477ca319219f7586a3e4bdd53be038f41757b
Ester-Tofik <325202349@mby.co.il>
2022-08-23 22:14:56 +0300

    maps work

--- src/index.tsx
+++ src/index.tsx
@@ -10,14 +10,15 @@ import reportWebVitals from './reportWebVitals';
 //   document.getElementById('root') as HTMLElement
 // );
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement!);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+// const rootElement = document.getElementById('root');
+// const root = createRoot(rootElement!);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+const root = ReactDOM.createRoot(document.getElementById("root")as HTMLElement);
+root.render(<App />);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
