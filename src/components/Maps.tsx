a7bc01ff8f45a89199724526141021bab53dca63
OraBenesh <orabenesh@gmail.com>
2022-08-24 00:22:36 +0300

    start markers

--- src/components/Maps.tsx
+++ src/components/Maps.tsx
@@ -168,6 +168,7 @@ import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/icons-material/Menu";
 import Search from "@mui/icons-material/Search";
 import Directions from "@mui/icons-material/Directions";
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
 
 const mapStyles = {
   width: "3vw",
@@ -210,6 +211,8 @@ const SimpleMap: React.FC = (props: any) => {
   const loading = open && options.length === 0;
   const [center, setCenter] = useState({ lat: 31.0461, lng: 34.8516 });
   const [zoom, setZoom] = useState(11);
+
+
   return (
     <>
       <Box sx={{ flexGrow: 1, height: "100%" }}>
@@ -232,14 +235,14 @@ const SimpleMap: React.FC = (props: any) => {
             </GoogleMapReact>
           </Grid>
           <Grid item xs={6} md={4}>
-            {/* <Typography
+            <Typography
               sx={{ textAlign: "center" }}
               gutterBottom
-              variant="h4"
+              variant="h6"
               component="div"
             >
               here you can search location business of your system
-            </Typography> */}
+            </Typography>
             <Paper
               component="form"
               sx={{
@@ -252,11 +255,6 @@ const SimpleMap: React.FC = (props: any) => {
               <IconButton sx={{ p: "10px" }} aria-label="menu">
                 <Menu />
               </IconButton>
-              {/* <InputBase
-            sx={{ ml: 1, flex: 1 }}
-            placeholder="Search Google Maps"
-            inputProps={{ "aria-label": "search google maps" }}
-          /> */}
               <Autocomplete
                 id="asynchronous-demo"
                 sx={{ width: 300 }}
@@ -276,8 +274,9 @@ const SimpleMap: React.FC = (props: any) => {
                 renderInput={(params) => (
                   <TextField
                     {...params}
-                    label="Asynchronous"
+                    placeholder="Search Google Maps"
                     InputProps={{
+                      "aria-label": "search google maps",
                       ...params.InputProps,
                       endAdornment: (
                         <React.Fragment>
@@ -311,3 +310,57 @@ const SimpleMap: React.FC = (props: any) => {
 };
 
 export default SimpleMap;
+
+// const locations = [
+//   { lat: -31.56391, lng: 147.154312 },
+//   { lat: -33.718234, lng: 150.363181 },
+//   { lat: -33.727111, lng: 150.371124 },
+//   { lat: -33.848588, lng: 151.209834 },
+//   { lat: -33.851702, lng: 151.216968 },
+//   { lat: -34.671264, lng: 150.863657 },
+//   { lat: -35.304724, lng: 148.662905 },
+//   { lat: -36.817685, lng: 175.699196 },
+//   { lat: -36.828611, lng: 175.790222 },
+//   { lat: -37.75, lng: 145.116667 },
+//   { lat: -37.759859, lng: 145.128708 },
+//   { lat: -37.765015, lng: 145.133858 },
+//   { lat: -37.770104, lng: 145.143299 },
+//   { lat: -37.7737, lng: 145.145187 },
+//   { lat: -37.774785, lng: 145.137978 },
+//   { lat: -37.819616, lng: 144.968119 },
+//   { lat: -38.330766, lng: 144.695692 },
+//   { lat: -39.927193, lng: 175.053218 },
+//   { lat: -41.330162, lng: 174.865694 },
+//   { lat: -42.734358, lng: 147.439506 },
+//   { lat: -42.734358, lng: 147.501315 },
+//   { lat: -42.735258, lng: 147.438 },
+//   { lat: -43.999792, lng: 170.463352 },
+// ];
+// const infoWindow = new google.maps.InfoWindow({
+//   content: "",
+//   disableAutoPan: true,
+// });
+//  // Create an array of alphabetical characters used to label the markers.
+//  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+//    // Add some markers to the map.
+//    const markers = locations.map((position, i) => {
+//     const label = labels[i % labels.length];
+//     const marker = new google.maps.Marker({
+//       position,
+//       label,
+//     });
+
+//     // markers can only be keyboard focusable when they have click listeners
+//     // open info window when marker is clicked
+//     marker.addListener("click", () => {
+//       infoWindow.setContent(label);
+//       infoWindow.open(SimpleMap, marker);
+//     });
+
+//     return marker;
+//   });
+//   const markerCluster = new MarkerClusterer({ SimpleMap, markers });
+
+//   // Add a marker clusterer to manage the markers.
+//   new MarkerClusterer({ markers, SimpleMap });
