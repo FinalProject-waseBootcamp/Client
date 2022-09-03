8e6c2cfdd09128c3d7c74cea045faa7ddb03bfb8
Ester-Tofik <325202349@mby.co.il>
2022-09-03 23:30:18 +0300

    aaa

---
+++ src/components/reCAPTCHA.tsx
@@ -0,0 +1,25 @@
+import { request } from 'https';
+import * as React from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
+import requestStore from '../store/requestStore';
+
+const Recaptcha: React.FC = () => {
+    function onChange(value: any) {
+     
+        console.log('Captcha value:', value);
+        debugger;
+        if(value){
+            requestStore.robot=true;
+        }
+
+    }
+    return (
+        <div className="App">
+            <ReCAPTCHA
+                sitekey="6LcYY7khAAAAAOL3e8Ah4gKrhlxlfykO615GjHaY"
+                onChange={onChange}
+            />
+        </div>
+    );
+};
+export default Recaptcha;
