d3e6fa3e174615f3f66b4ef6b5218e429fd16527
OraBenesh <orabenesh@gmail.com>
2022-09-05 10:11:18 +0300

    good morning

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,11 @@ import AdminSystems from "./components/system/AdminSystems";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Maps from "./components/Maps/Maps";
-import  MyAutocomplete from './components/Maps/AutoComplete';
-import { onAuthStateChanged,getAuth } from "firebase/auth";
+import MyAutocomplete from "./components/Maps/AutoComplete";
+import { onAuthStateChanged, getAuth } from "firebase/auth";
 import userStore from "./store/userStore";
 import AddMarker from "./components/Maps/AddMarker";
 import Map from "./components/Maps/Map";
-
 
 let theme = createTheme();
 theme = createTheme(theme, {
@@ -36,12 +35,11 @@ let auth = getAuth();
 let user = auth.currentUser;
 console.log(user);
 onAuthStateChanged(auth, (user) => {
-  debugger
+  debugger;
   auth = getAuth();
   user = auth.currentUser;
   userStore.setUser(user);
 });
-
 
 function App() {
   return (
@@ -56,15 +54,12 @@ function App() {
             <Route path="/resetPassword" element={<ResetPassword />} />
             <Route path="/user" element={<User />} />
             <Route path="/addSystem" element={<AddSystem />} />
-            <Route path="/system/welcome/:name/:uid" element={<MySystem />}>
-              {/* <Route path="*" element={<System />} /> */}
-            </Route>
+            <Route path="/system/welcome/:name/:uid" element={<MySystem />} />
             <Route path="/adminSystems" element={<AdminSystems />} />
             <Route path="/editSystem" element={<EditSystem />} />
             <Route path="/maps" element={<Maps />} />
             <Route path="/auto" element={<MyAutocomplete />} />
             <Route path="/add" element={<AddMarker />} />
-
           </Routes>
         </Router>
         <img src={require("./utils/logo.jpg")} id="logo" alt="logo"></img>
